e81ce0aea448cfad295d30aa6a6b0d2a0d9b644f
todorr92 <todorovicslobodan10@gmail.com>
2020-04-22 19:53:25 +0000

    added beforeEach function

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,14 +1,23 @@
 describe("Calculator", function(){
+
+    beforeEach(function() {
+        calc = new Calculator();
+    });
+    
     describe("Addition tests", function(){
         it("Should return 42", function(){
-            expect(addition(20, 22)).toBe(42);
+            calc.add(20);
+            calc.add(22);
+            expect(calc.value).toBe(42);
         });
         it("Should return 26", function(){
-            expect(addition(7, 19)).toBe(26);
+            calc.add(7);
+            calc.add(19);
+            expect(calc.value).toBe(26);
         });
         it("Should return an error if we don't supply two numbers", function(){
             spyOn(window, "alert");
-            addition("Hitchhiker", "Guide");
+            calc.add("Hitchhiker");
             expect(window.alert).toHaveBeenCalledWith("Error!");
         })
     });
